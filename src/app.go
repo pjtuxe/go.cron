474f50fb1a95f9d6d9cb8cb62826dc6acc4d77fd
keszi000 <keszi000@gmail.com>
2021-04-13 00:46:25 +0200

    initial

--- src/app.go
+++ src/app.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func log(msg string) {
+	fmt.Println(msg)
+}
+
+func errorHandler(msg string, err error) {
+	if err != nil {
+		fmt.Printf(msg+"%s\n", err)
+		panic(err)
+	}
+}
+
+func getJobs(apiUrl string) {
+	response, err := http.Get(apiUrl)
+	errorHandler("The HTTP request failed with error", err)
+	data, _ := ioutil.ReadAll(response.Body)
+	log(string(data))
+}
 
 func main() {
-	fmt.Printf("go.cron started")
+	log("go.cron started")
+	getJobs("https://httpbin.org/ip")
 }
