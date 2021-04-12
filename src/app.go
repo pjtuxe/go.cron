b1dcb6a89de4a1a223391cdd5d338400d76d0b94
keszi000 <keszi000@gmail.com>
2021-04-13 01:32:21 +0200

    base getJobs Function

--- src/app.go
+++ src/app.go
@@ -1,25 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
-	"go/types"
 	"io/ioutil"
 	"net/http"
 )
 
 var (
-	apiUrl = flag.String("u", "https://httpbin.org/ip", "API URL")
+	apiUrl = flag.String("u", "https://6074d24f066e7e0017e7a5d5.mockapi.io/api/jobs/test", "API URL")
 )
 
 type job struct {
-	name                string
-	image               string
-	entrypoint          string
-	commandcron_pattern string
-	variables           types.Array
-	command             types.Array
+	Name       string `json:"name"`
+	ID         string `json:"id"`
+	Image      string `json:"image"`
+	Entrypoint string `json:"entrypoint"`
+	Command    string `json:"command"`
+	Variables  struct {
+		Key string `json:"key"`
+	} `json:"variables"`
+	Cronpattern string `json:"cronPattern"`
 }
+
+type jobs []job
 
 func log(msg string) {
 	fmt.Println(msg)
@@ -32,14 +37,17 @@ func errorHandler(msg string, err error) {
 	}
 }
 
-func getJobs(apiUrl string) {
+func getJobs(apiUrl string) jobs {
 	response, err := http.Get(apiUrl)
-	errorHandler("The HTTP request failed with error", err)
 	data, _ := ioutil.ReadAll(response.Body)
-	log(string(data))
+	errorHandler("The HTTP request failed with error", err)
+	var Response jobs
+	parseErr := json.Unmarshal(data, &Response)
+	errorHandler("Parse error", parseErr)
+	return Response
 }
 
 func main() {
 	log("go.cron started")
-	getJobs(*apiUrl)
+	fmt.Printf("%+v\n", getJobs(*apiUrl))
 }
